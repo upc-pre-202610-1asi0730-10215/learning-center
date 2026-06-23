384cb7b12621dda18ac81710909e68b52c0d9408
Angel Velasquez <pcisavel@upc.edu.pe>
2026-06-23 18:53:49 -0500

    feat(iam): simplify authentication guard by removing next parameter and adding navigation logging

--- src/iam/infrastructure/authentication.guard.js
+++ src/iam/infrastructure/authentication.guard.js
@@ -5,14 +5,14 @@ import useIamStore from "../application/iam.store.js";
  *
  * @param {import('vue-router').RouteLocationNormalized} to - Target route.
  * @param {import('vue-router').RouteLocationNormalized} from - Current route.
- * @param {import('vue-router').NavigationGuardNext} next - Guard continuation callback.
  * @returns {{name: string} | boolean } - The result, either redirecting to the sign-in page or allowing navigation.
  */
-export const authenticationGuard = (to, from, next) => {
+export const authenticationGuard = (to, from) => {
     const store = useIamStore();
     const isAnonymous = !store.isSignedIn;
     const publicRoutes = ['/iam/sign-in', '/iam/sign-up', '/about', '/page-not-found'];
     const routeRequiresToBeAuthenticated = !publicRoutes.includes(to.path);
     if (isAnonymous && routeRequiresToBeAuthenticated) return {name: 'iam-sign-in'};
     console.log(`Navigating from ${from.name} to ${to.name}`);
+    return true;
 }
