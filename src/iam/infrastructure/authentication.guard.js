f68cc7eda075e0653ba5ae865308ba76ab7d9d52
Angel Velasquez <pcisavel@upc.edu.pe>
2026-06-23 18:50:01 -0500

    fix(iam): update authentication guard to return redirect object instead of using next callback.

--- src/iam/infrastructure/authentication.guard.js
+++ src/iam/infrastructure/authentication.guard.js
@@ -6,13 +6,12 @@ import useIamStore from "../application/iam.store.js";
  * @param {import('vue-router').RouteLocationNormalized} to - Target route.
  * @param {import('vue-router').RouteLocationNormalized} from - Current route.
  * @param {import('vue-router').NavigationGuardNext} next - Guard continuation callback.
- * @returns {void}
+ * @returns {{name: string} | boolean } - The result, either redirecting to the sign-in page or allowing navigation.
  */
 export const authenticationGuard = (to, from, next) => {
     const store = useIamStore();
     const isAnonymous = !store.isSignedIn;
     const publicRoutes = ['/iam/sign-in', '/iam/sign-up', '/about', '/page-not-found'];
     const routeRequiresToBeAuthenticated = !publicRoutes.includes(to.path);
-    if (isAnonymous && routeRequiresToBeAuthenticated) return next({ name: 'iam-sign-in'});
-    else next();
+    if (isAnonymous && routeRequiresToBeAuthenticated) return {name: 'iam-sign-in'};
 }
