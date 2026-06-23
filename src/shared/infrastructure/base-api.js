5e873bbb4963e2ce4db3343426b85b9e307ee35e
Angel Velasquez <pcisavel@upc.edu.pe>
2026-06-23 18:50:19 -0500

    feat(api): add iam interceptor to http client for enhanced request handling.

--- src/shared/infrastructure/base-api.js
+++ src/shared/infrastructure/base-api.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import {iamInterceptor} from "../../iam/infrastructure/iam.interceptor.js";
 const platformApi = import.meta.env.VITE_LEARNING_PLATFORM_API_URL;
 
 /**
@@ -18,6 +19,8 @@ export class BaseApi {
             headers: { 'Content-Type': 'application/json',
             'Access-Control-Allow-Origin': '*'}
         });
+        // Add the IAM interceptor to the HTTP client
+        this.#http.interceptors.request.use(iamInterceptor);
     }
 
     /**
